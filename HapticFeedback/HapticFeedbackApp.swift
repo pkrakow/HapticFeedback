77f34bf56923bd562e18d15e533e3077af1139fb
Paul Krakow <pkrakow@gmail.com>
2021-01-17 09:11:21 -0800

    Initial Commit

---
+++ HapticFeedback/HapticFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticFeedbackApp.swift
+//  HapticFeedback
+//
+//  Created by Paul on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct HapticFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
