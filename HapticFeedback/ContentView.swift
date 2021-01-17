421fe8b7b8c058d5e491b82b895a97be947f96e6
Paul Krakow <pkrakow@gmail.com>
2021-01-17 10:55:02 -0800

    working haptic feedback with sound as well

--- HapticFeedback/ContentView.swift
+++ HapticFeedback/ContentView.swift
@@ -6,11 +6,68 @@
 //
 
 import SwiftUI
+import UIKit
+
+let impact = UIImpactFeedbackGenerator() // 1
+
+func onePressed(_ sender: Any) {
+    impact.impactOccurred() // 2
+}
+
+let selection = UISelectionFeedbackGenerator()
+
+func twoPressed(_ sender: Any) {
+    selection.selectionChanged()
+}
+
+let notification = UINotificationFeedbackGenerator()
+
+func successPressed(_ sender: Any) {
+    notification.notificationOccurred(.success)
+}
+
+func warningPressed(_ sender: Any) {
+    notification.notificationOccurred(.warning)
+}
+
+func errorPressed(_ sender: Any) {
+    notification.notificationOccurred(.error)
+}
+
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Button("impact") {
+                onePressed(impact)
+                playKeyPressedOne()
+            } .padding()
+            
+            Button("selection") {
+                twoPressed(selection)
+                playSound(sound: "Bell-Tree", type: "mp3")
+            } .padding()
+            
+            Button("success") {
+                successPressed(notification)
+                playKeyPressedOne()
+            } .padding()
+            
+            Button("warning") {
+                warningPressed(notification)
+                playKeyPressedTwo()
+            } .padding()
+            
+            Button("error") {
+                errorPressed(notification)
+                playKeyPressedThree()
+            } .padding()
+        }
+    }
+    
+    func noOp() -> Void {
+        return
     }
 }
 
